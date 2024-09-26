a534d0d6f0acc958e0339b96c17e48cfc50c90fb
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-26 14:44:24 -0300

    Add route for deleting a service by id

---
+++ src/routes/Services/delete-service.ts
@@ -0,0 +1,43 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { prisma } from '../../prisma/prisma-client'
+
+export async function DeleteService(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .delete('/service/:serviceId', {
+        schema: {
+            params: z.object({
+                serviceId: z.string().cuid(),
+            }),
+            summary: 'Delete a service by id',
+            tags: ['Services'],
+        }
+    }, async (request, reply) => {
+        const { serviceId } = request.params
+        const userId = request.getCurrentUserId()
+
+        if (!userId) {
+            return reply.status(401).send({ error: "Usuário não autenticado" });
+        }
+
+        const service = await prisma.service.findFirst({
+            where: {
+                id: serviceId,
+            },
+        })
+
+        if (!service) {
+            return reply.status(404).send({ error: "Serviço não encontrado" });
+        }
+
+        await prisma.service.delete({
+            where: {
+                id: serviceId,
+            },
+        })
+
+        return reply.status(204).send({ message: 'Serviço deletado com sucesso' })
+    })
+}
